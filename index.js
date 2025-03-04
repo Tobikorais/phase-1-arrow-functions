5e50c1bcee8cf7b30ea57654062ca38110c3f090
Andrew Tobiko <andrew.tobiko1@student.moringaschool.com>
2025-03-04 10:56:38 +0300

    Adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+};
